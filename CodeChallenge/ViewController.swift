5efc4d0e8b1c6cd48aa71124c9717284882093aa
Pierluigi Galdi <info@pigigaldi.com>
2018-01-18 15:51:43 +0100

    Initial Commit

---
+++ CodeChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CodeChallenge
+//
+//  Created by Pierluigi Galdi on 18/01/18.
+//  Copyright © 2018 Pierluigi Galdi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
